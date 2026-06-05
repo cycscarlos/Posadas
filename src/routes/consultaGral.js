cce5c680f7ba78b546a5723789b3e7c2c72b473b
cycscarlos <cycs.ingenieria@gmail.com>
2026-06-05 11:37:01 -0400

    fix: data entry tambien accede a consultaGral

--- src/routes/consultaGral.js
+++ src/routes/consultaGral.js
@@ -4,6 +4,6 @@ const ctrl_consultaGral = require("../controllers/ctrl_consultaGral");
 const authorize = require("../middlewares/authorize");
 
 // Ruta para mostrar todos los registros en una consulta general de la BDD
-router.get("/consultaGral", authorize(["admin"]), ctrl_consultaGral.consulta);
+router.get("/consultaGral", authorize(["admin", "data entry"]), ctrl_consultaGral.consulta);
 
 module.exports = router;
